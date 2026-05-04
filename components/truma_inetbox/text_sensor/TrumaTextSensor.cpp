040c52e8d2855d9f80ae062ff26ecc8aa3f7fb0b
bertel2020 <72860283+bertel2020@users.noreply.github.com>
2026-05-04 19:37:30 +0200

    Update TrumaTextSensor.cpp

--- components/truma_inetbox/text_sensor/TrumaTextSensor.cpp
+++ components/truma_inetbox/text_sensor/TrumaTextSensor.cpp
@@ -7,18 +7,15 @@ namespace truma_inetbox {
 static const char *const TAG = "truma_inetbox.text_sensor";
 
 void TrumaTextSensor::setup() {
-  this->parent_->get_clock()->add_on_message_callback([this](const StatusFrameClock *status_clock) {
+  this->parent_->get_clock()->add_on_message_callback([this](const StatusFrameClock *c) {
     switch (this->type_) {
       case TRUMA_TEXT_SENSOR_TYPE::CLOCK: {
         char buf[6];
-        snprintf(buf, sizeof(buf), "%02d:%02d",
-                 status_clock->clock_hour,
-                 status_clock->clock_minute);
+        snprintf(buf, sizeof(buf), "%02d:%02d", c->clock_hour, c->clock_minute);
         this->publish_state(buf);
         break;
       }
-      default:
-        break;
+      default: break;
     }
   });
 }
