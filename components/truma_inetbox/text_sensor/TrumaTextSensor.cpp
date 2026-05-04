364802343903171291559e7ff6a0f8dfe245e9e0
bertel2020 <72860283+bertel2020@users.noreply.github.com>
2026-05-04 14:00:52 +0200

    Create TrumaTextSensor.cpp

---
+++ components/truma_inetbox/text_sensor/TrumaTextSensor.cpp
@@ -0,0 +1,32 @@
+#include "TrumaTextSensor.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace truma_inetbox {
+
+static const char *const TAG = "truma_inetbox.text_sensor";
+
+void TrumaTextSensor::setup() {
+  this->parent_->get_clock()->add_on_message_callback([this](const StatusFrameClock *status_clock) {
+    switch (this->type_) {
+      case TRUMA_TEXT_SENSOR_TYPE::CLOCK: {
+        char buf[6];
+        snprintf(buf, sizeof(buf), "%02d:%02d",
+                 status_clock->clock_hour,
+                 status_clock->clock_minute);
+        this->publish_state(buf);
+        break;
+      }
+      default:
+        break;
+    }
+  });
+}
+
+void TrumaTextSensor::dump_config() {
+  LOG_TEXT_SENSOR("", "Truma Text Sensor", this);
+  ESP_LOGCONFIG(TAG, "  Type '%s'", enum_to_c_str(this->type_));
+}
+
+}  // namespace truma_inetbox
+}  // namespace esphome
