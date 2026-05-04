a93b476045ba81b9c6daa5760a4c56a4f137004b
bertel2020 <72860283+bertel2020@users.noreply.github.com>
2026-05-04 14:01:33 +0200

    Create __init__.py

---
+++ components/truma_inetbox/text_sensor/__init__.py
@@ -0,0 +1,55 @@
+from esphome.components import text_sensor
+import esphome.config_validation as cv
+import esphome.codegen as cg
+from esphome.const import (
+    CONF_ID,
+    CONF_TYPE,
+    CONF_ICON,
+)
+from .. import truma_inetbox_ns, CONF_TRUMA_INETBOX_ID, TrumaINetBoxApp
+
+DEPENDENCIES = ["truma_inetbox"]
+CODEOWNERS = ["@bertel2020"]
+
+CONF_CLASS = "class"
+
+TrumaTextSensor = truma_inetbox_ns.class_(
+    "TrumaTextSensor", text_sensor.TextSensor, cg.Component
+)
+
+TRUMA_TEXT_SENSOR_TYPE_dummy_ns = truma_inetbox_ns.namespace("TRUMA_TEXT_SENSOR_TYPE")
+
+CONF_SUPPORTED_TYPE = {
+    "CLOCK": {
+        CONF_CLASS: TRUMA_TEXT_SENSOR_TYPE_dummy_ns.CLOCK,
+        CONF_ICON: "mdi:clock-outline",
+    },
+}
+
+
+def set_default_based_on_type():
+    def set_defaults_(config):
+        sensor_type = CONF_SUPPORTED_TYPE[config[CONF_TYPE]]
+        if CONF_ICON in sensor_type and CONF_ICON not in config:
+            config[CONF_ICON] = sensor_type[CONF_ICON]
+        return config
+    return set_defaults_
+
+
+CONFIG_SCHEMA = text_sensor.text_sensor_schema().extend(
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(TrumaTextSensor),
+        cv.GenerateID(CONF_TRUMA_INETBOX_ID): cv.use_id(TrumaINetBoxApp),
+        cv.Required(CONF_TYPE): cv.enum(CONF_SUPPORTED_TYPE, upper=True),
+    })
+).extend(cv.COMPONENT_SCHEMA)
+
+FINAL_VALIDATE_SCHEMA = set_default_based_on_type()
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await text_sensor.register_text_sensor(var, config)
+    await cg.register_parented(var, config[CONF_TRUMA_INETBOX_ID])
+    cg.add(var.set_type(CONF_SUPPORTED_TYPE[config[CONF_TYPE]][CONF_CLASS]))
