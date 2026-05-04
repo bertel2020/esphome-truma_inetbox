c60bd9f839c8a8f83960535518e3ba962621e662
bertel2020 <72860283+bertel2020@users.noreply.github.com>
2026-05-04 18:02:12 +0200

    Update TrumaSensor.h

--- components/truma_inetbox/sensor/TrumaSensor.h
+++ components/truma_inetbox/sensor/TrumaSensor.h
@@ -24,35 +24,27 @@ enum class TRUMA_SENSOR_TYPE {
   TIMER_WATER_TEMPERATURE,
   CLOCK_HOUR,
   CLOCK_MINUTE,
+  CP_PLUS_DISPLAY_STATUS,
+  HEATING_STATUS,
 };
 
 #ifdef ESPHOME_LOG_HAS_CONFIG
 static const char *enum_to_c_str(const TRUMA_SENSOR_TYPE val) {
   switch (val) {
-    case TRUMA_SENSOR_TYPE::CURRENT_ROOM_TEMPERATURE:
-      return "CURRENT_ROOM_TEMPERATURE";
-    case TRUMA_SENSOR_TYPE::CURRENT_WATER_TEMPERATURE:
-      return "CURRENT_WATER_TEMPERATURE";
-    case TRUMA_SENSOR_TYPE::TARGET_ROOM_TEMPERATURE:
-      return "TARGET_ROOM_TEMPERATURE";
-    case TRUMA_SENSOR_TYPE::TARGET_WATER_TEMPERATURE:
-      return "TARGET_WATER_TEMPERATURE";
-    case TRUMA_SENSOR_TYPE::HEATING_MODE:
-      return "HEATING_MODE";
-    case TRUMA_SENSOR_TYPE::ELECTRIC_POWER_LEVEL:
-      return "ELECTRIC_POWER_LEVEL";
-    case TRUMA_SENSOR_TYPE::ENERGY_MIX:
-      return "ENERGY_MIX";
-    case TRUMA_SENSOR_TYPE::OPERATING_STATUS:
-      return "OPERATING_STATUS";
-    case TRUMA_SENSOR_TYPE::HEATER_ERROR_CODE:
-      return "HEATER_ERROR_CODE";
-    case TRUMA_SENSOR_TYPE::CLOCK_HOUR:
-      return "CLOCK_HOUR";
-    case TRUMA_SENSOR_TYPE::CLOCK_MINUTE:
-      return "CLOCK_MINUTE";
-    default:
-      return "";
+    case TRUMA_SENSOR_TYPE::CURRENT_ROOM_TEMPERATURE:  return "CURRENT_ROOM_TEMPERATURE";
+    case TRUMA_SENSOR_TYPE::CURRENT_WATER_TEMPERATURE: return "CURRENT_WATER_TEMPERATURE";
+    case TRUMA_SENSOR_TYPE::TARGET_ROOM_TEMPERATURE:   return "TARGET_ROOM_TEMPERATURE";
+    case TRUMA_SENSOR_TYPE::TARGET_WATER_TEMPERATURE:  return "TARGET_WATER_TEMPERATURE";
+    case TRUMA_SENSOR_TYPE::HEATING_MODE:              return "HEATING_MODE";
+    case TRUMA_SENSOR_TYPE::ELECTRIC_POWER_LEVEL:      return "ELECTRIC_POWER_LEVEL";
+    case TRUMA_SENSOR_TYPE::ENERGY_MIX:                return "ENERGY_MIX";
+    case TRUMA_SENSOR_TYPE::OPERATING_STATUS:          return "OPERATING_STATUS";
+    case TRUMA_SENSOR_TYPE::HEATER_ERROR_CODE:         return "HEATER_ERROR_CODE";
+    case TRUMA_SENSOR_TYPE::CLOCK_HOUR:                return "CLOCK_HOUR";
+    case TRUMA_SENSOR_TYPE::CLOCK_MINUTE:              return "CLOCK_MINUTE";
+    case TRUMA_SENSOR_TYPE::CP_PLUS_DISPLAY_STATUS:    return "CP_PLUS_DISPLAY_STATUS";
+    case TRUMA_SENSOR_TYPE::HEATING_STATUS:            return "HEATING_STATUS";
+    default:                                           return "";
   }
 }
 #endif  // ESPHOME_LOG_HAS_CONFIG
@@ -66,8 +58,6 @@ class TrumaSensor : public Component, public sensor::Sensor, public Parented<Tru
 
  protected:
   TRUMA_SENSOR_TYPE type_;
-
- private:
 };
 
 }  // namespace truma_inetbox
