bc7e08b0a36fb2ee5cf10276697da4c1a2ac0b84
bertel2020 <72860283+bertel2020@users.noreply.github.com>
2026-05-06 12:43:02 +0200

    Update TrumaRoomClimate.h

--- components/truma_inetbox/climate/TrumaRoomClimate.h
+++ components/truma_inetbox/climate/TrumaRoomClimate.h
@@ -21,17 +21,9 @@ namespace esphome {
     void set_visual_temperature_step(float value) { this->visual_temperature_step_ = value; }
 
     void set_supported_modes(const std::set<climate::ClimateMode> &modes);
-    void add_supported_fan_mode(climate::ClimateFanMode fan_mode) {
-      this->supported_fan_modes_.insert(fan_mode);
-    }
 
    protected:
     std::set<esphome::climate::ClimateMode> supported_modes_;
-    std::set<esphome::climate::ClimateFanMode> supported_fan_modes_{
-      climate::CLIMATE_FAN_OFF,
-      climate::CLIMATE_FAN_LOW,
-      climate::CLIMATE_FAN_HIGH,
-    };
     float visual_min_temperature_{5.0};
     float visual_max_temperature_{30.0};
     float visual_temperature_step_{0.5};
