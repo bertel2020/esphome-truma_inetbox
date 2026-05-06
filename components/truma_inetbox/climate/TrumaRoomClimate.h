a5b49322d85f82aeec78735a5243e2452753a1d6
bertel2020 <72860283+bertel2020@users.noreply.github.com>
2026-05-06 11:24:50 +0200

    Update TrumaRoomClimate.h

--- components/truma_inetbox/climate/TrumaRoomClimate.h
+++ components/truma_inetbox/climate/TrumaRoomClimate.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <set>
+#include <string>
 #include "esphome/components/climate/climate.h"
 #include "esphome/core/component.h"
 #include "esphome/components/truma_inetbox/TrumaiNetBoxApp.h"
@@ -15,14 +16,13 @@ namespace esphome {
     void control(const climate::ClimateCall &call) override;
     void setup() override;
     climate::ClimateTraits traits() override;
-  
-  
+
     void set_visual_min_temperature(float value) { this->visual_min_temperature_ = value; }
     void set_visual_max_temperature(float value) { this->visual_max_temperature_ = value; }
     void set_visual_temperature_step(float value) { this->visual_temperature_step_ = value; }
 
     void set_supported_modes(const std::set<climate::ClimateMode> &modes);
-  
+
    protected:
     std::set<esphome::climate::ClimateMode> supported_modes_;
     float visual_min_temperature_{5.0};
