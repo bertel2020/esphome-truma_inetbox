c06e6933fa0b99c11cfb4dc1d744007f7b1cf18c
bertel2020 <72860283+bertel2020@users.noreply.github.com>
2026-05-11 15:23:41 +0200

    Update TrumaEnums.h

--- components/truma_inetbox/TrumaEnums.h
+++ components/truma_inetbox/TrumaEnums.h
@@ -89,7 +89,7 @@ enum class OperatingStatus : uint8_t {
   OPERATING_STATUS_UNSET              = 0x0,
   OPERATING_STATUS_OFF                = 0x0,
   OPERATING_STATUS_WARNING            = 0x1,
-  OPERATING_STATUS_ON_STANDBY         = 0x4,
+  OPERATING_STATUS_START_OR_COOL_DOWN = 0x4,
   OPERATING_STATUS_ON_5               = 0x5,
   OPERATING_STATUS_ON_6               = 0x6,
   OPERATING_STATUS_ON_7               = 0x7,
